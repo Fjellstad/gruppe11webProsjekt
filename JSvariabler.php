9acd8774e13129ad6b1e28eef4b86afbcf78bcd3
Mads-power <Mads>
2017-05-04 18:57:27 +0200

    php + java script
    jeg har lagt til noen nye .php filer som jeg må ha på kart siden. har
    også begynt å legge til det jeg trenger på kart siden

---
+++ JSvariabler.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mads-power
+ * Date: 04.05.2017
+ * Time: 16.05
+ */
+?>
+<script>
+var type =
+
+    function type()
+    {
+        document . getElementById("buttonBolig");
+        document . getElementById("buttonTransport");
+        document . getElementById("buttonMat");
+        document . getElementById("buttonHelse");
+        document . getElementById("buttonEvent");
+    }
+
+
+</script>
