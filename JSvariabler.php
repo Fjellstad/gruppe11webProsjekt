4c4bec7cb97beef0dc12685801bc0c1f01adf6b4
Mads-power <Mads>
2017-05-10 12:20:15 +0200

    jScript kart
    ss

--- JSvariabler.php
+++ JSvariabler.php
@@ -7,16 +7,32 @@
  */
 ?>
 <script>
-    var helse = ;
+    //sender til kart.php filen
+    (function (window,google) {
 
-    function type()
-    {
+    //skal lage en listener for click knapper
+    google.maps.event.addListener(map.gMap,'click', function (e) {
+        alert('')
+    })
+
+   //fun
+   var clicked = (function type(){
+        function clicked() {
+
+
+
         document . getElementById("buttonBolig");
         document . getElementById("buttonTransport");
         document . getElementById("buttonMat");
         document . getElementById("buttonHelse");
         document . getElementById("buttonEvent");
-    }
+        }
+        return clicked();
+   }());
 
+    clicked.create = function () {
+        return new clicked();
+    };
 
+    }(window, window.kart))
 </script>
