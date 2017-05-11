b3e55a818e8c62348ea189e900f4682b8ccae6c8
Mads-power <Mads>
2017-05-11 14:41:52 +0200

    jScript i kart
    sda

--- kart.php
+++ kart.php
@@ -24,32 +24,15 @@ include ("JSvariabler.php");
 
 </head>
 <body>
+
 <script>
-
-    function downloadUrl(search.php,callback) {
-        var request = window.ActiveXObject ?
-            new ActiveXObject('Microsoft.XMLHTTP') :
-            new XMLHttpRequest;
-
-        request.onreadystatechange = function() {
-            if (request.readyState == 4) {
-                request.onreadystatechange = doNothing;
-                callback(request, request.status);
-            }
-        };
-
-        request.open('GET', url, true);
-        request.send(null);
-    }
-
-</script>
-<script>
-function test() {
-var user = "<?php echo $user;?>"
+/*function test() {
+var user = "
 alert("welcome " + user)
 }
-window.addEventListener('load', test);
+window.addEventListener('load', test);*/
 </script>
+
 <div id="header">
     <div id="headerLogo">
     <a href="index.html"><img id="wLogo" src="css/bilder/Logo150px.png"></a>
@@ -102,7 +85,50 @@ window.addEventListener('load', test);
     <div id="mapContainer">
         <script>
             function initMap() {
-                var uluru = {lat: 59.922336, lng: 10.751484};
+                var map = new google.maps.Map(document.getElementById('mapContainer'), {
+                    center: new google.maps.LatLng(-33.863276, 151.207977),
+                    zoom: 12
+                });
+                var infoWindow = new google.maps.InfoWindow;
+
+                // Change this depending on the name of your PHP or XML file
+                downloadUrl('search.php', function(data) {
+                    var xml = data.responseXML;
+                    var markers = xml.documentElement.getElementsByTagName('kart_webprosjekt');
+                    Array.prototype.forEach.call(markers, function(markerElem) {
+                        var id = markerElem.getAttribute('id');
+                        var name = markerElem.getAttribute('name');
+                        var address = markerElem.getAttribute('address');
+                        var type = markerElem.getAttribute('type');
+                        var point = new google.maps.LatLng(
+                            parseFloat(markerElem.getAttribute('lat')),
+                            parseFloat(markerElem.getAttribute('lng')));
+
+                        var infowincontent = document.createElement('div');
+                        var strong = document.createElement('strong');
+                        strong.textContent = name
+                        infowincontent.appendChild(strong);
+                        infowincontent.appendChild(document.createElement('br'));
+
+                        var text = document.createElement('text');
+                        text.textContent = address
+                        infowincontent.appendChild(text);
+                        var icon = customLabel[type] || {};
+                        var marker = new google.maps.Marker({
+                            map: map,
+                            position: point,
+                            label: icon.label
+                        });
+                        marker.addListener('click', function() {
+                            infoWindow.setContent(infowincontent);
+                            infoWindow.open(map, marker);
+                        });
+                    });
+                });
+            }
+
+
+            /*function initMap() {
                 var map = new google.maps.Map(document.getElementById('mapContainer'), {
                     zoom: 15,
                     center: uluru
@@ -110,10 +136,25 @@ window.addEventListener('load', test);
                 var marker = new google.maps.Marker({
                     position: uluru,
                     map: map
-                });
+                });*/
 
 
 
+            }
+            function downloadUrl(search.php,callback) {
+                var request = window.ActiveXObject ?
+                    new ActiveXObject('Microsoft.XMLHTTP') :
+                    new XMLHttpRequest;
+
+                request.onreadystatechange = function() {
+                    if (request.readyState == 4) {
+                        request.onreadystatechange = doNothing;
+                        callback(request, request.status);
+                    }
+                };
+
+                request.open('GET', url, true);
+                request.send(null);
             }
 
         </script>
