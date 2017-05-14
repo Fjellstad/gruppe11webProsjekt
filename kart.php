8f2a3724756a7d0cf39f75291e51613fb38811cc
Mads-power <mr.kolkin@gmail.com>
2017-05-14 21:58:00 +0200

    kart, css og html
    måtte fikse sånn at javascriptet funket

--- kart.php
+++ kart.php
@@ -54,79 +54,88 @@
         </div>
     </div>
 
-    <form id="navigatorContainer">
+    <!--<div id="navigatorContainer">
         <a href="">
-            <div id="buttonBolig" class="button"><p onclick="Clicked()">Bolig</p></div>
-        </a>
-        <a href="">
-            <div id="buttonTransport" class="button"><p onclick="type()">Transport</p></div>
-        </a>
-        <a href="">
-            <div id="buttonMat" class="button"><p onclick="type()">Mat</p></div>
+
         </a>
         <a href="">
 
-            <div id="buttonHelse" class="button"><p onclick="type()">Helse</p></div>
+        </a>
+        <a href="">
 
         </a>
         <a href="">
-            <div id="buttonEvent" class="button"><p onclick="type()">Event</p></div>
+
         </a>
+        <a href="">
 
-    </form>
+        </a>-->
 
+    </div>
+    <div id="buttonHelse" class="button"> <button onclick="click()">Helse</button> </div>
+    <div id="buttonEvent" class="button"><button onclick="click()">Event</button></div>
+    <div id="buttonMat" class="button"><button onclick="click()">Mat</button></div>
+    <div id="buttonTransport" class="button"><button onclick="click()">Transport</button></div>
+    <div id="buttonBolig" class="button"><button onclick="click()">Bolig</button></div>
     <div id="map">
 
         <script>
             var map;
             var myLatLng ={lat: 59.923321,lng: 10.752427};
             var cords = [
-                ['Bondi Beach',  59.923426, 10.751531 , 4],
-                ['Coogee Beach', 59.923098, 10.751214, 5],
-                ['Coogee Beach', 59.922633, 10.751010, 5]
+                ['Vitusapotek Vulkan', 59.923526,10.750798, 4],
+                ['Apotek 1 Grünerløkka', 59.923496, 10.757125, 5],
+                ['Apotek 1 Legevakten', 59.917079,10.758761, 6],
+                ['Sio Athletica Vulkan',59.923170,10.752157,7],
+                ['Fitness24seven',59.921711,10.757207,8],
+                ['SATS Schous plass',59.919106,10.760330,9],
+                ['EVO Grunerløkka ',59.920048,10.759860,10],
+                ['legevakten grunerløkka ',59.917079,10.758761,11]
+            ]
+
+            var bCords = [
+                ['b1',59.923393, 10.750128,7],
+                ['b2',59.922215, 10.749624,8]
 
             ]
-            function initMap() {
 
+
+            function initMap() {
                 map = new google.maps.Map(document.getElementById('map'), {
                     center: myLatLng,
-                    zoom: 16
-
+                    zoom: 14
                 });
 
                 var marker,i;
+                document.getElementById("buttonHelse").addEventListener("click", click);
 
+                for (i = 0; i < bCords.length; i++) {
+                    marker = new google.maps.Marker({
+                        position: new google.maps.LatLng(bCords[i][1], bCords[i][2]),
+                        map: map,
+                        visible: false
+                    })
+                }
 
-                for(i = 0; i < cords.length; i++){
+                for (i = 0; i < cords.length; i++) {
                     marker = new google.maps.Marker({
                         position: new google.maps.LatLng(cords[i][1], cords[i][2]),
                         map: map,
-                        visible: true
-
-
+                        visible: false
                     })
                 }
-                function myFunction() {
-                    var x = document.getElementById("button").onclick;
 
+                /*function click() {
+                 for (var j = 0; j < marker.length; j++)
+                 marker[i].onclick =  marker.visible(true);
+                 }*/
+                //fungerer på 1 marker
+                function click() {
                     marker.setVisible(true);
                 }
-
-
-
-
-
-
-
-
-
-
-
-
             }
+
         </script>
-
-
         <script async defer
                 src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDvFzSBmeyXX-eBNAu-5hRcpXv2eH-clzc &callback=initMap">
         </script>
