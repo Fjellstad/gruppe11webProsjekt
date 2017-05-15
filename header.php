35cbc1e4162e20dd920c7b5c3406e8cee18bef0e
Naraysia <ersvil16@student.westerdals.no>
2017-05-15 22:16:33 +0200

    Fikset litt småpirk + header.php

--- header.php
+++ header.php
@@ -1,110 +1,100 @@
 <style>
-    #header{
-        position: relative;
-        height: 200px;
-        width: 100%;
-        background-color: rgb(42, 42, 42);
-        top: 0px;
-        box-shadow: 0px 1px 8px 2px #1f1f1f;
-    }
-    #headerLogo{
-        position: absolute;
-        height: 150px;
-        width: 150px;
-        left: 0;
-        right: 0;
-        margin: 0 auto;
-        top: 0;
-    }
+#header{
+    position: relative;
+    height: 200px;
+    width: 100%;
+    background-color: rgb(42, 42, 42);
+    top: 0px;
+    box-shadow: 0px 1px 8px 2px #1f1f1f;
+}
+    
+#headerLogo{
+    position: absolute;
+    height: 150px;
+    width: 150px;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+    top: 0;
+}
 
-    #menu{
-        position: absolute;
-        width: 1200px;
-        height: 50px;
-        top: 75px;
-        left: 0;
-        right: 0;
-        margin: 0 auto;
-    }
+.navbar ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    display: inline-block;
+    text-align: center;
 
-    p, .menuText{
-        color: #FFF;
-        font-family: sans-serif;
-    }
+}
 
-    #bolig{
-        position: absolute;
-        left: 125px;
-    }
+.navbar li {
+    margin-top: 80px;
+    margin-left: 20px;
+    margin-right: 20px;
 
-    #kart{
-        position: absolute;
-        left: 250px;
+}
 
-        width: 300px;
-        height: 200px;
-    }
+.navbar li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
 
-    #transport{
-        position: absolute;
-        left: 375px;
-    }
+li a:hover {
+    border-bottom: solid thin #ffd066;
+}
 
-    #eventer{
-        position: absolute;
-        right: 125px;
-    }
 
-    #helse{
-        position: absolute;
-        right: 260px;
-    }
-    #mat{
-        position: absolute;
-        right: 375px;
-    }
+.navbar {
+    width: 1270px;
+    margin: 0 auto;
+    font-family: sans-serif;
+}
 
-    /* Linker/bilder til sosiale medier */
-    #facebook{
-        position: absolute;
-        top: 15px;
-        right: 50px;
-        height: 25px;
-        width: 25px;
-    }
 
-    #instagram{
-        position: absolute;
-        top: 15px;
-        right: 105px;
-        height: 25px;
-        width: 25px;
-    }
+.nav-middle {
+    width: 230px;
+}
 
-    #twitter{
-        position: absolute;
-        top: 11px;
-        right: 150px;
-        height: 32px;
-        width: 32px;
-    }
+.nav-right{
+    float: right;
+    margin-top: 100px;
+    width: 520px;
+
+} 
+
+.nav-right li {
+    float: left;
+}
+
+.nav-left {
+    float: left;
+    width: 520px;
+}
+
+.nav-left li {
+    float: right;
+}
+
 
 </style>
 
 
 <div id="header">
-    <div id="headerLogo">
-        <a href="index.html"><img id="headerLogo" src="bilder/Logo150px.png"></a>
+        <div class="navbar">
+            <ul class="nav-left">
+                <li>  <a href="http://www.vg.no">Transport</a></li>
+                <li>  <a href="http://www.vg.no">Kart</a></li>
+                <li>  <a href="http://www.vg.no">Bolig</a></li>
+            </ul>        <ul class="nav-middle">
+            <img src="bilder/Logo150px.png">
+       </ul>
+        <ul class="nav-right">
+            <li>  <a href="http://www.vg.no">Mat og drikke</a></li>
+            <li>  <a href="http://www.vg.no">Helse</a></li>
+            <li>  <a href="http://www.vg.no">Eventer</a></li>
+        </ul> 
+        </div>
     </div>
-    <a href="https://www.facebook.com"><img id="facebook" src="bilder/facebook.png"></a>
-    <a href="https://www.twitter.com"><img id="twitter" src="bilder/twitter.png"></a>
-    <a href="https://www.instagram.com"><img id="instagram" src="bilder/instagram.png"></a>
-    <div id="menu" class="menuText">
-        <a href="www.vg.no"><p id="bolig">Bolig</p></a>
-        <a href="www.facebook.no"><p id="kart">Kart</p></a>
-        <a href="www.vg.no"><p id="transport">Transport</p></a>
-        <a href="www.vg.no"><p id="mat">Mat og drikke</p></a>
-        <a href="www.vg.no"><p id="helse">Helse</p></a>
-        <a href="www.vg.no"><p id="eventer">Eventer</p></a>
-    </div>
-</div>
