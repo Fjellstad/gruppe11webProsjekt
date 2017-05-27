c10763c66dd309131e7bd3a161bf85d65b7749b9
Mads-power <mr.kolkin@gmail.com>
2017-05-27 14:02:20 +0200

    mat og drikke path fix

--- undersider/matogdrikke.php
+++ undersider/matogdrikke.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="">
         <meta name="author" content="Vilde Martine Ersdal"> <!-- Author -->
-        <link href="css/matogdrikke.css" type="text/css" rel="stylesheet">
+        <link href="/css/matogdrikke.css" type="text/css" rel="stylesheet">
         <title>Mat og drikke - Westerdals</title>
 
     </head>
@@ -23,7 +23,7 @@
             
         <div id="mathallenBox" class="contentBox"> <!-- MATHALLENBOX START -->
             <h2 class="title">Mathallen</h2>
-            <img class="pics" src="bilder/mathallen.png">
+            <img class="pics" src="/bilder/mathallen.png">
                 <p class="contentText">Rundt Campus Vulkan er matkulturen stor. Ikke bare kan du velge mellom restauranter og barer, men om du er ekstra glad i god mat kan ta turen innom Mathallen. Her finner du spesialbutikker, kafeer og spisesteder samtidig som Mathallen byr på kurs, messer og konkurranser. 
                 <br><br>Mathallen er en unik plass du ikke vil gå glipp av og er for alle. Fra den nysgjerrige nybegynner, til den ambisiøse kjøkkensjef.
                 <br> <br>Besøk mathallens hjemmeside <a href="https://mathallenoslo.no/">her</a>!</p>
@@ -33,14 +33,14 @@
             
         <div id="restuBarBox" class="contentBox"> <!-- RESTURANT AND BAR-BOX START -->
             <h2 class="title">Resturanter og barer</h2>
-            <img class="pics" src="bilder/chef.png">
+            <img class="pics" src="/bilder/chef.png">
                 <p class="contentText">På Vulkan kryr det med restauranter og barer. Du vil finne et eksepsjonelt bra uteliv både på dag og kveldstid. Døgnvill, Smelteverket og Lucky Bird er bare noen av restaurantene som Vulkan har å by på.
                 <br> <br> Vil du opp i høyden burde du dra på BAR Vulkan hvor de har uteservering av mat og drikke på en stor takterrasse med utsikt over byen. Å spise på BAR Vulkan skal være sosialt og spennende. Kokkene henter inspirasjon fra hele verden og alle retter er lagd for å deles.</p>
         </div> <!-- RESTURANT AND BAR-BOX END -->
             
         <div id="dagligvareBox" class="contentBox"> <!-- DAGLIGVARE-BOX START -->
             <h2 class="title">Dagligvarebutikker</h2>
-            <img class="pics" src="bilder/grocereis.png">
+            <img class="pics" src="/bilder/grocereis.png">
                 <p class="contentText">Dersom du ønsker å handle i en vanlig dagligvarebutikk behøver du ikke bevege deg langt. 
                 Rema 1000 ligger rundt et lite hjørne fra Campus Vulkan.
                 Med en 5-10 minutters gåtur finner du også andre matbutikker som Kiwi og Coop Prix.
